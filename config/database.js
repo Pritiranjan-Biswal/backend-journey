9808b8ef5685d235d333b8331f7aabf72f532745
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-06-16 22:02:52 +0530

    l

---
+++ config/database.js
@@ -0,0 +1,14 @@
+const mongoose =  require("mongoose");
+
+require("dotenv").config();
+const dbConnect=() => {
+    mongoose.connect(process.env.DATANASE_URL)
+    .then(()=>console.log("DB ka connection successful"))
+    .catch((error) => {
+        console.log("Issue in DB connection")
+        console.error(error.message);
+        process.exit(1);
+        
+    })
+}
+module.exports=dbConnect;
