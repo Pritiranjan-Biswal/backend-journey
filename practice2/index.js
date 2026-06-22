04371c8e58177d3340089f39eeb99b5b0d623a39
Pritiranjan Biswal <pritiranjanbiswal46@gmail.com>
2026-06-22 20:09:11 +0530

    new

---
+++ practice2/index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const app=express();
+
+require("dotenv").config();
+const PORT= process.env.PORT || 3000;
+
+// middleware
+
+app.use(express.json());
+
+const blog=require("./routes/blog")
+
+//mount
+
+app.use("/api/v1", blog)
+
+
+const connectWithDb=require("./config/database");
+connectWithDb();
+
+//start the server
+
+app.listen(PORT, ()=> {
+    console.log(`App is started at ${PORT}`);
+    
+})
+
+app.get("/" ,(req, res) => {
+    req.send(<h3>This is my home page </h3>)
+})
